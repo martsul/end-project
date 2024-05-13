660a95c24f9e15ab229118efa29c8fe884ab4071
martsul <sasham9321@mail.ru>
2024-05-14 00:20:08 +0300

    90% js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,117 @@
+// SWIPER //
+
+let swiper = (
+  backBtn,
+  nextBtn,
+  cards,
+  timeEl,
+  filedChangingPosition,
+  timingFuncOn,
+  fieldForSwipe,
+  startSwipe,
+  thereAreArrow
+) => {
+  let swiperIteration = 0;
+  let leftPosition = 0;
+  let swipeTurning;
+
+  if (thereAreArrow) {
+    let disabledButton = () => {
+      switch (swiperIteration) {
+        case 0:
+          backBtn.disabled = true;
+          nextBtn.disabled = false;
+          break;
+
+        case cards.length - 1:
+          backBtn.disabled = false;
+          nextBtn.disabled = true;
+          break;
+
+        default:
+          backBtn.disabled = false;
+          nextBtn.disabled = false;
+          break;
+      }
+    };
+
+    disabledButton();
+
+    backBtn.addEventListener("click", () => {
+      swiperIteration--;
+      leftPosition += 100;
+
+      filedChangingPosition.style.left = `${leftPosition}%`;
+
+      disabledButton();
+    });
+
+    nextBtn.addEventListener("click", () => {
+      swiperIteration++;
+      leftPosition -= 100;
+
+      filedChangingPosition.style.left = `${leftPosition}%`;
+
+      disabledButton();
+    });
+  }
+
+  if (window.innerWidth <= startSwipe && timingFuncOn) {
+    let lastIteration = 0;
+
+    timeEl[0].classList.add("time-animation-on");
+
+    setInterval(() => {
+      lastIteration = swiperIteration;
+
+      if (swiperIteration === cards.length - 1) {
+        swiperIteration = 0;
+        leftPosition = 0;
+      } else {
+        swiperIteration++;
+        leftPosition -= 100;
+      }
+
+      timeEl[lastIteration].classList.remove("time-animation-on");
+      timeEl[swiperIteration].classList.add("time-animation-on");
+
+      filedChangingPosition.style.left = `${leftPosition}%`;
+    }, 3500);
+
+    fieldForSwipe.forEach((element) => {
+      element.addEventListener("touchstart", (event) => {
+        swipeTurning = event.touches[0].clientX;
+      });
+
+      element.addEventListener("touchend", (event) => {
+        swipeTurning -= event.changedTouches[0].screenX;
+
+        if (swipeTurning > 10 && swiperIteration !== cards.length - 1) {
+          lastIteration = swiperIteration;
+          swiperIteration++;
+          leftPosition -= 100;
+
+          timeEl[lastIteration].classList.remove("time-animation-on");
+          timeEl[swiperIteration].classList.add("time-animation-on");
+
+          filedChangingPosition.style.left = `${leftPosition}%`;
+        } else if (swipeTurning < -10 && swiperIteration !== 0) {
+          lastIteration = swiperIteration;
+          swiperIteration--;
+          leftPosition += 100;
+
+          timeEl[lastIteration].classList.remove("time-animation-on");
+          timeEl[swiperIteration].classList.add("time-animation-on");
+
+          filedChangingPosition.style.left = `${leftPosition}%`;
+        }
+      });
+    });
+  }
+};
+
+// SWIPER //
+
 // // POP-UP //
 
 document
@@ -18,12 +132,36 @@ document.querySelector(".pop-up__close").addEventListener("click", (event) => {
 
 // // UNIVERSITIES //
 
-let filtersButtonOff =
-  '<span>Фильтры</span><svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="button-icon"><path d="M19.2569 -1.90735e-06H0.820313C0.388794 -1.90735e-06 0.0390625 0.34973 0.0390625 0.781248C0.0390625 2.95685 0.97168 5.03387 2.59781 6.47934L6.01929 9.52042C6.61255 10.0478 6.95282 10.8055 6.95282 11.5994V19.2178C6.95282 19.8404 7.64862 20.2136 8.16727 19.8677L12.7765 16.795C12.9938 16.6501 13.1244 16.4063 13.1244 16.145V11.5994C13.1244 10.8055 13.4647 10.0478 14.0579 9.52042L17.4793 6.47934C19.1054 5.03387 20.038 2.95685 20.038 0.781248C20.038 0.34973 19.6883 -1.90735e-06 19.2569 -1.90735e-06ZM16.4412 5.31143L13.0199 8.35266C12.0934 9.17633 11.5619 10.3597 11.5619 11.5993V15.7269L8.51517 17.758V11.5994C8.51517 10.3597 7.98371 9.17633 7.05719 8.35266L3.63587 5.31158C2.53937 4.3367 1.83945 3.00095 1.65207 1.56235H18.425C18.2376 3.00095 17.5378 4.3367 16.4412 5.31143Z"></path></svg>';
-let filtersButtonOn =
-  '<img src="img/menu-close.png" alt="close" class="universities__filters-item-img">';
-
 if (document.querySelector("#universities")) {
+  let cards = document.querySelectorAll(".universities__card");
+  let backBtn, nextBtn, filedChangingPosition, imgs;
+
+  cards.forEach((element) => {
+    backBtn = element.querySelector(".universities__card-img-before");
+    nextBtn = element.querySelector(".universities__card-img-next");
+    filedChangingPosition = element.querySelector(
+      ".universities__slider-field"
+    );
+    imgs = element.querySelectorAll(".universities__img-container");
+
+    swiper(
+      backBtn,
+      nextBtn,
+      imgs,
+      NaN,
+      filedChangingPosition,
+      false,
+      NaN,
+      NaN,
+      true
+    );
+  });
+
+  let filtersButtonOff =
+    '<span>Фильтры</span><svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="button-icon"><path d="M19.2569 -1.90735e-06H0.820313C0.388794 -1.90735e-06 0.0390625 0.34973 0.0390625 0.781248C0.0390625 2.95685 0.97168 5.03387 2.59781 6.47934L6.01929 9.52042C6.61255 10.0478 6.95282 10.8055 6.95282 11.5994V19.2178C6.95282 19.8404 7.64862 20.2136 8.16727 19.8677L12.7765 16.795C12.9938 16.6501 13.1244 16.4063 13.1244 16.145V11.5994C13.1244 10.8055 13.4647 10.0478 14.0579 9.52042L17.4793 6.47934C19.1054 5.03387 20.038 2.95685 20.038 0.781248C20.038 0.34973 19.6883 -1.90735e-06 19.2569 -1.90735e-06ZM16.4412 5.31143L13.0199 8.35266C12.0934 9.17633 11.5619 10.3597 11.5619 11.5993V15.7269L8.51517 17.758V11.5994C8.51517 10.3597 7.98371 9.17633 7.05719 8.35266L3.63587 5.31158C2.53937 4.3367 1.83945 3.00095 1.65207 1.56235H18.425C18.2376 3.00095 17.5378 4.3367 16.4412 5.31143Z"></path></svg>';
+  let filtersButtonOn =
+    '<img src="img/menu-close.png" alt="close" class="universities__filters-item-img">';
+
   let filtersCurtain = document
     .querySelector(".universities__filters")
     .querySelectorAll(".universities__filters-chapter");
@@ -76,8 +214,12 @@ if (document.querySelector("#universities")) {
   });
 
   document.querySelector(".more").addEventListener("click", (event) => {
-    document.querySelectorAll(".curtain-off").forEach((e) => {
+    document.querySelectorAll(".universities__card-container").forEach((e) => {
       e.classList.toggle("curtain-all");
+      document
+        .querySelector(".more")
+        .querySelector(".button-icon")
+        .classList.toggle("button-icon-all");
     });
   });
 
@@ -162,19 +304,56 @@ if (document.querySelector("#universities")) {
           filtersButtonOn;
       }
     });
-    
-    let staticAllCard = document.querySelectorAll(".universities__card");
 
-    document.querySelector(".universities__search-filed").addEventListener("keyup", event => {
-      let currentAllCard = staticAllCard;
-      allCard[0].remove();
-      console.log([...allCard]);
-    })
+  let staticAllCard = document.querySelectorAll(".universities__card");
+  let searchField = document.querySelector(".universities__search-filed");
+
+  searchField.addEventListener("keyup", (event) => {
+    let thereAreCards = false;
+    let searchFieldValue = new RegExp(`${searchField.value}`, "i");
+
+    staticAllCard.forEach((element) => {
+      if (
+        element
+          .querySelector(".universities__card-name")
+          .innerHTML.match(searchFieldValue)
+      ) {
+        thereAreCards = true;
+
+        element.removeAttribute("style");
+      } else {
+        element.setAttribute("style", "display: none;");
+      }
+
+      if (thereAreCards) {
+        document.querySelector(".more").removeAttribute("style");
+        document
+          .querySelector(".universities__nothing")
+          .removeAttribute("style");
+      } else {
+        document.querySelector(".more").style.display = "none";
+        document.querySelector(".universities__nothing").style.display =
+          "inline-block";
+      }
+    });
+  });
 }
 
 // UNIVERSITY //
 
 if (document.querySelector("#university")) {
+  swiper(
+    document.querySelector("#left-gallery-button"),
+    document.querySelector("#right-gallery-button"),
+    document.querySelectorAll(".university__slider-card"),
+    document.querySelectorAll(".university__time-item"),
+    document.querySelector(".university__slider-field"),
+    true,
+    document.querySelectorAll(".university__img"),
+    1030,
+    true
+  );
+
   document
     .querySelector(".university__button")
     .addEventListener("click", (event) => {
@@ -224,6 +403,27 @@ if (document.querySelector("#news")) {
 
 if (document.querySelector("#students")) {
   let buttonMore = document.querySelector(".more");
+  let allCards = document.querySelectorAll(".students__card");
+  let cardsBackBtn, cardsNextBtn, oneOfCards, swipingField;
+
+  allCards.forEach((element) => {
+    cardsBackBtn = element.querySelector(".students__before-btn");
+    cardsNextBtn = element.querySelector(".students__next-btn");
+    oneOfCards = element.querySelectorAll(".students__img-container");
+    swipingField = element.querySelector(".students__slider-field");
+
+    swiper(
+      cardsBackBtn,
+      cardsNextBtn,
+      oneOfCards,
+      "",
+      swipingField,
+      false,
+      NaN,
+      NaN,
+      true
+    );
+  });
 
   buttonMore.addEventListener("click", (event) => {
     document
@@ -404,12 +604,20 @@ forms.forEach((form) => {
           let monthValueForYear =
             labelDate[ind - 1].querySelector("input").value;
 
-          errorConditions(
-            currentDate.value <= new Date().getFullYear() &&
-              currentDate.value > 0,
-            event,
-            element
-          );
+          if (ind === 2) {
+            errorConditions(
+              currentDate.value <= new Date().getFullYear() &&
+                currentDate.value > 0,
+              event,
+              element
+            );
+          } else {
+            errorConditions(
+              currentDate.value >= new Date().getFullYear(),
+              event,
+              element
+            );
+          }
 
           if (currentDate.value % 4 !== 0 && monthValueForYear === "2") {
             errorConditions(
@@ -476,6 +684,11 @@ forms.forEach((form) => {
 
 // HEADER CURTAIN //
 
+let open =
+  '<img src="img/menu-open.png" alt="menu" class="header__menu-button-img">';
+let close =
+  '<img src="img/menu-close.png" alt="menu" class="header__menu-button-img">';
+
 document
   .querySelector(".header__menu-button")
   .addEventListener("click", (event) => {
@@ -483,6 +696,12 @@ document
       document.querySelector(".header").classList.toggle("header-on-home");
     } else {
       document.querySelector(".header").classList.toggle("header-on");
+    }
+
+    if (document.querySelector(".header__menu-button").innerHTML === close) {
+      document.querySelector(".header__menu-button").innerHTML = open;
+    } else {
+      document.querySelector(".header__menu-button").innerHTML = close;
     }
 
     document
@@ -502,11 +721,49 @@ document
 
 // APPLICATION //
 
-// HOME_FEEDBACK //
+// HOME //
 
 if (document.querySelector("#home")) {
-  document.querySelector(".preview__btn").addEventListener("click", (event) => {
-    document.location = "#questions";
+  swiper(
+    NaN,
+    NaN,
+    document.querySelectorAll(".preview__card"),
+    document.querySelectorAll(".preview__time-item"),
+    document.querySelector(".preview__slider-field"),
+    true,
+    document.querySelectorAll(".preview__card"),
+    Infinity,
+    false
+  );
+
+  swiper(
+    document.querySelector("#left-universities-button"),
+    document.querySelector("#right-universities-button"),
+    document.querySelectorAll(".universities__slider-card"),
+    document.querySelectorAll(".universities__time-item"),
+    document.querySelector(".universities__slider-field"),
+    true,
+    document.querySelectorAll(".universities__card-container"),
+    1030,
+    true
+  );
+
+  swiper(
+    document.querySelector("#left-reviews-button"),
+    document.querySelector("#right-reviews-button"),
+    document.querySelectorAll(".reviews__card"),
+    document.querySelectorAll(".reviews__time-item"),
+    document.querySelector(".reviews__slider-field"),
+    true,
+    document.querySelectorAll(".reviews__card-container"),
+    1030,
+    true
+  );
+
+  document.querySelectorAll(".preview__btn").forEach((element) => {
+    element.addEventListener("click", (event) => {
+      document.location = "#questions";
+    });
   });
 
   let answerCard = document.querySelectorAll(".answer__card");
@@ -523,3 +780,21 @@ if (document.querySelector("#home")) {
 }
 
 // HOME_FEEDBACK //
+
+// ARTICLE //
+
+if (document.querySelector("#article")) {
+  swiper(
+    document.querySelector(".same__border-left"),
+    document.querySelector(".same__border-right"),
+    document.querySelectorAll(".same__card"),
+    document.querySelectorAll(".same__time-item"),
+    document.querySelector(".same-slider-field"),
+    true,
+    document.querySelectorAll(".same__card"),
+    1030,
+    true
+  );
+}
+
+// ARTICLE //
